086801a0cb011d09d902edafd8ec2fb747b7b74f
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2015-09-29 20:47:45 +0530

    adds new spider for same scraper but diff method

--- login_u/login_u/spiders/spiders.py
+++ login_u/login_u/spiders/spiders.py
@@ -2,7 +2,6 @@ from scrapy.contrib.spiders.init import InitSpider
 from scrapy.http import Request, FormRequest
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import Rule
-from scrapy.contrib.spiders import CrawlSpider, Rule
 
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
@@ -11,6 +10,46 @@ from login_u.items import myspiderBotItem
 
 class LinkedPySpider(InitSpider):
     name = 'myspider'
+    allowed_domains = ['linkedin.com']
+    login_page = 'https://www.linkedin.com/uas/login'
+    start_urls = ["http://www.linkedin.com/csearch/results?type=companies&keywords=&pplSearchOrigin=GLHD&pageKey=member-home&search=Search#facets=pplSearchOrigin%3DFCTD%26keywords%3D%26search%3DSubmit%26facet_CS%3DC%26facet_I%3D80%26openFacets%3DJO%252CN%252CCS%252CNFR%252CF%252CCCR%252CI"]
+
+    def init_request(self):
+        #"""This function is called before crawling starts."""
+        return Request(url=self.login_page, callback=self.login)
+
+    def login(self, response):
+        #"""Generate a login request."""
+        return FormRequest.from_response(response,
+                    formdata={'session_key': 's.mahavarkar@gmail.com', 'session_password': 'Sujitvm123!'},
+                    callback=self.check_login_response)
+
+    def check_login_response(self, response):
+        #"""Check the response returned by a login request to see if we aresuccessfully logged in."""
+        if "Sign Out" in response.body:
+            self.log("\n\n\nSuccessfully logged in. Let's start crawling!\n\n\n")
+            # Now the crawling can begin..
+
+            return self.initialized() # ****THIS LINE FIXED THE LAST PROBLEM*****
+
+        else:
+            self.log("\n\n\nFailed, Bad times :(\n\n\n")
+            # Something went wrong, we couldn't log in, so nothing happens.
+
+    def parse(self, response):
+        self.log("\n\n\n We got data! \n\n\n")
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('//ol[@id=\'result-set\']/li')
+        items = []
+        for site in sites:
+            item = myspiderBotItem()
+            item['title'] = site.select('h2/a/text()').extract()
+            item['link'] = site.select('h2/a/@href').extract()
+            items.append(item)
+        return items
+
+class LinkedPySpider1(InitSpider):
+    name = 'myspider1'
     allowed_domains = ['linkedin.com']
     login_page = 'https://www.linkedin.com/uas/login'
    # start_urls = ["http://www.linkedin.com/csearch/results?type=companies&keywords=&pplSearchOrigin=GLHD&pageKey=member-home&search=Search#facets=pplSearchOrigin%3DFCTD%26keywords%3D%26search%3DSubmit%26facet_CS%3DC%26facet_I%3D80%26openFacets%3DJO%252CN%252CCS%252CNFR%252CF%252CCCR%252CI"]
@@ -28,9 +67,9 @@ class LinkedPySpider(InitSpider):
         dont_filter=True
         )
 
-  #  def init_request(self):
-    #"""This function is called before crawling starts."""
-  #      return Request(url=self.login_page, callback=self.login)
+    # def initialized(self):
+    #     """This function is called before crawling starts."""
+    #     return Request(url=self.login_page, callback=self.login)
     def initialized(self):
         """This function is called before crawling starts."""
         return Request(url='https://www.linkedin.com/nhome/', callback=self.parse_item)
